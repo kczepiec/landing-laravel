b3430e06a57ca48e674b916f636e6baa1a25d986
Krzysztof Czepiec <kontakt@krzysztofczepiec.pl>
2019-12-20 02:03:46 +0100

    routing

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
-
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 // Route::post('/client', function (Request $request) {
 //     dd('test');
